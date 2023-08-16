38034f670a749fe43d62d7d698bc05f08fe461e2
hanal-exactSpace <hanal.h@exactspace.co>
2023-08-16 12:39:33 +0530

    css styles

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -5,31 +5,14 @@ import ftimage from '../assets/images/football.jpg'
 function Signup() {
   return (
     <div className='row' >
-      <div className='right-side'>
-        <div className='title'>
-          <h1>oops tech</h1>
-          
-        </div>
-        <form>
-          <input
-            type='text'
-            placeholder='First Name'
-          />
-          <input
-            type='text'
-            placeholder='Last Name'
-          />
-          <input
-            type='email'
-            placeholder='Email'
-          />
-          <input
-            type='password'
-            placeholder='Password'
-          />
-          <button type='submit'>Submit</button>
-        </form>
-      </div>
+     <div className='reg_section'>
+        <a href="">Hello 123</a>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+
+     </div>
     </div>
   );
 }
